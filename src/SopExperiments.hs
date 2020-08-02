0227ab4a8dbd07bef4b0b26913f74ab12936f274
Andreas Källberg <anka.213@gmail.com>
2020-08-02 21:00:21 +0200

    Do a bunch of experiments, including adding a parser

--- src/SopExperiments.hs
+++ src/SopExperiments.hs
@@ -1,3 +1,11 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+{-# LANGUAGE DerivingStrategies #-}
 {- |
 Copyright: (c) 2020 Andreas Källberg
 SPDX-License-Identifier: MIT
@@ -7,9 +15,75 @@ Experiments with generics-sop
 -}
 
 module SopExperiments
-       ( someFunc
+       ( module SopExperiments
+       , Proxy(..)
        ) where
 
+import qualified GHC.Generics as GHC
+import Generics.SOP
+-- import Data.Proxy
 
 someFunc :: IO ()
 someFunc = putStrLn ("someFunc" :: String)
+
+--- $> :browse Generics.SOP
+
+data Example = Ex1 Int () | Ex2 { _ex2 :: Char}
+  deriving stock (Eq, Show)
+  deriving stock (GHC.Generic)
+
+instance Generic Example
+instance HasDatatypeInfo Example
+
+px :: Proxy t
+px = Proxy
+
+-- $> :kind! Code Example
+-- '[ '[Int, ()], '[(), Char]]
+
+-- $> datatypeInfo @Example px
+
+--- $> :kind! DatatypeInfoOf Example
+
+type CE = Code Example
+
+-- ex :: DatatypeInfo '[ '[Int, ()], '[(), Char]]
+ex :: DatatypeInfo CE
+ex = datatypeInfo @Example px
+
+conInfo :: NP ConstructorInfo CE
+conInfo = case ex of
+  ADT _moduleName _name c _strictnessInfo -> c
+
+-- $> conInfo
+-- Constructor "Ex1" :* Record "Ex2" (FieldInfo "_ex2" :* Nil) :* Nil
+
+newtype Use f b a = Use (f a -> b)
+
+-- Consume a sum type with a product of functions
+nsCase :: NP (Use f r) arr -> NS f arr -> r
+nsCase (Use f :* _) (Z x) = f x
+nsCase (_ :* fs) (S xs) = nsCase fs xs
+nsCase Nil nope = case nope of {}
+
+useEx :: Example -> String
+useEx = nsCase handleIt . unSOP . from
+
+handleIt :: NP (Use (NP I) String) CE
+handleIt = Use ex1 :* Use ex2 :* Nil
+
+wrap :: (f a -> r) -> (f -.-> K r) a
+wrap f = Fn $ K . f
+
+handleIt' :: NP (NP I -.-> K String) CE
+handleIt' = wrap ex1 :* wrap ex2 :* Nil
+
+-- useEx' :: Example -> Int
+useEx' :: Example -> String
+useEx' = hcollapse . hap handleIt' . unSOP . from @Example
+
+ex1 :: NP I '[Int, ()] -> String
+ex1 (I n :* I () :* Nil) = "Ex1, Int: " ++ show n
+
+ex2 :: NP I '[Char] -> String
+ex2 (I c :* Nil) = "Ex2. Char: " ++ show c
---
+++ src/SopParser.hs
@@ -0,0 +1,104 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+module SopParser (module SopParser) where
+
+import qualified GHC.Generics as GHC
+import Generics.SOP
+import Text.ParserCombinators.ReadP
+import Control.Applicative (Alternative((<|>)))
+import Test.QuickCheck (arbitrary, elements, Gen, Arbitrary)
+import Control.Monad (liftM)
+import Generics.SOP.NP (sequence_NP, cpure_NP)
+
+data FooBar = Foo | Bar
+  deriving stock (Eq, Show)
+  deriving stock (GHC.Generic)
+
+instance Generic FooBar
+instance HasDatatypeInfo FooBar
+
+--- $> :browse Text.ParserCombinators.ReadP
+
+-- mkParse :: ConstrInfos a -> ReadP a
+-- mkParse (Constructor s :* _xs) = string s
+-- mkParse Nil = pfail
+
+-- mkParse :: IsEnumType a => Proxy a -> ConstrInfos a -> NP (K (ReadP (SOP I (Code a)))) (Code a)
+-- mkParse _ = hmap $ K . (_ <$) . string . constructorName
+
+-- mkParse :: IsEnumType a => Proxy a -> ConstrInfos a -> NP (K (ReadP ())) (Code a)
+-- mkParse _ = hmap $ K . (() <$) . string . constructorName
+
+type IsEnumCode c = All ((~) '[]) c
+
+mkParse :: IsEnumCode c => NP ConstructorInfo c -> NP (K (ReadP ())) c
+mkParse = hmap $ K . (() <$) . string . constructorName
+
+-- mkParse2 :: All ((~) '[]) c => NP (K (ReadP ())) c -> ReadP (SOP I c)
+-- mkParse2 (K x :* xs) = SOP (Z Nil) <$ x <|> SOP . S . unSOP <$> mkParse2 xs
+-- mkParse2 Nil = pfail
+
+mkParse2 :: IsEnumCode c => NP (K (ReadP ())) c -> ReadP (NS (K ()) c)
+mkParse2 (K x :* xs) = Z (K ()) <$ x <|> S <$> mkParse2 xs
+mkParse2 Nil = pfail
+
+mkParse2' :: IsEnumCode c => NP (K (ReadP ())) c -> ReadP (NS (K ()) c)
+mkParse2' = choice . fmap hsequenceK . apInjs_NP
+
+parse :: (IsEnumType a, HasDatatypeInfo a) => Proxy a -> ReadP a
+parse p = fmap enumTypeTo . mkParse2 . mkParse $ getConstructors p
+
+-- mkParseG0 :: ConstructorInfo a -> ReadP (NP I a)
+-- mkParseG0 (Constructor name) = undefined name
+-- mkParseG0 (Infix name b _n) = undefined name b
+-- mkParseG0 (Record a b) = _
+
+parseOne :: Read a => ReadP a
+parseOne = readS_to_P reads
+
+mkParseG1 :: All2 Read c => NP ConstructorInfo c -> NP (ReadP :.: NP I) c
+mkParseG1 = hcmap (Proxy @(All Read)) $ Comp . mkParseG1Inner
+
+mkParseG1Inner :: All Read c => ConstructorInfo c -> ReadP (NP I c)
+mkParseG1Inner info =
+    string (constructorName info)
+    *> sequence_NP (cpure_NP @Read Proxy parseOne)
+
+-- getIt :: ReadP (NP I a)
+-- getIt = _
+
+-- mkParseG1 = hmap $ Comp . _ . string . constructorName
+
+mkParseG2 :: SListI c => NP (ReadP :.: NP I) c -> ReadP (NS (NP I) c)
+mkParseG2 = choice . fmap hsequence' . apInjs_NP
+
+type Parsable c = All2 Read c
+
+gparse :: (Parsable (Code a), HasDatatypeInfo a) => Proxy a -> ReadP a
+gparse = fmap (to . SOP) . mkParseG2 . mkParseG1 . getConstructors 
+
+type ConstrInfos a =  NP ConstructorInfo (Code a)
+
+getConstructors :: HasDatatypeInfo t => proxy t -> NP ConstructorInfo (Code t)
+getConstructors = constructorInfo . datatypeInfo
+
+parseFoo :: ReadP FooBar
+parseFoo = parse Proxy
+
+gparseFoo :: ReadP FooBar
+gparseFoo = gparse Proxy
+
+-- $> readP_to_S (gparse (Proxy @([Int]))) ": 1 [2,3]"
+
+garbitrary :: forall a. (Generic a, All2 Arbitrary (Code a)) => Gen a
+garbitrary = liftM to $ hsequence =<< elements (apInjs_POP $ hcpure p arbitrary)
+  where
+    p :: Proxy Arbitrary
+    p = Proxy
